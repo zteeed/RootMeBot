f26fd34dcc74976d120cd3d94ceca33afd5c11c7
zteeed <zteeed@minet.net>
2020-02-29 17:26:55 +0000

    fixup

--- bot/constants.py
+++ bot/constants.py
@@ -1,4 +1,3 @@
-bot_channel = 'root-me-news'
 emoji1 = 'dab'
 emoji2 = 'ok_hand'
 emoji3 = 'thinking'
@@ -6,7 +5,6 @@ emoji4 = 'thinking'
 emoji5 = 'thumbsdown'
 limit_size = 1000
 medals = [':first_place:', ':second_place:', ':third_place:']
-token = 'token'
 timeout = 15
 URL = 'http://api:3000'
 DEFAULT_LANG = 'en'
